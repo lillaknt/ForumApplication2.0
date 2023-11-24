5245b0bf43754c4a7de0e904976bb6dee8d3f742
lillaknt <lillaknt@gmail.com>
2023-11-24 08:35:26 +0100

    Authentication Imlemented

--- BlazorApp/Program.cs
+++ BlazorApp/Program.cs
@@ -1,8 +1,10 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using BlazorApp;
+using BlazorApp.Auth;
 using HttpClients.ClientInterfaces;
 using HttpClients.Implementation;
+using Microsoft.AspNetCore.Components.Authorization;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -17,7 +19,9 @@ builder.Services.AddScoped(sp =>
 builder.Services.AddScoped<IUserService, UserHttpClient>();
 builder.Services.AddScoped<IPostService, PostHttpClient>();
 
+builder.Services.AddScoped<IAuthService, JwtAuthService>();
+builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthProvider>();
 
-
+builder.Services.AddAuthorizationCore();
 
 await builder.Build().RunAsync();
